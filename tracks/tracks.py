ea02ef642c408eff21ae1af6bd76bba93e8a3951
Gursimran <gursimransinghsaluja@gmail.com>
2021-09-12 12:51:38 +0200

    Updated and modified Tracks.py

--- tracks/tracks.py
+++ tracks/tracks.py
@@ -7,10 +7,16 @@ cur = conn.cursor()
 # Make some fresh tables using executescript()
 cur.executescript('''
 DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Genre;
 DROP TABLE IF EXISTS Album;
 DROP TABLE IF EXISTS Track;
 
 CREATE TABLE Artist (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Genre (
     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     name    TEXT UNIQUE
 );
@@ -26,6 +32,7 @@ CREATE TABLE Track (
         AUTOINCREMENT UNIQUE,
     title TEXT  UNIQUE,
     album_id  INTEGER,
+    genre_id  INTEGER,
     len INTEGER, rating INTEGER, count INTEGER
 );
 ''')
@@ -37,6 +44,7 @@ if ( len(fname) < 1 ) : fname = 'Library.xml'
 # <key>Track ID</key><integer>369</integer>
 # <key>Name</key><string>Another One Bites The Dust</string>
 # <key>Artist</key><string>Queen</string>
+
 def lookup(d, key):
     found = False
     for child in d:
@@ -47,21 +55,24 @@ def lookup(d, key):
 
 stuff = ET.parse(fname)
 all = stuff.findall('dict/dict/dict')
-print('Dict count:', len(all))
+
+#print('Dict count:', len(all))
+
 for entry in all:
     if ( lookup(entry, 'Track ID') is None ) : continue
 
     name = lookup(entry, 'Name')
     artist = lookup(entry, 'Artist')
     album = lookup(entry, 'Album')
+    genre = lookup(entry, 'Genre')
     count = lookup(entry, 'Play Count')
     rating = lookup(entry, 'Rating')
     length = lookup(entry, 'Total Time')
 
-    if name is None or artist is None or album is None : 
+    if name is None or artist is None or genre is None or album is None : 
         continue
 
-    print(name, artist, album, count, rating, length)
+    #print(name, artist, album, genre, count, rating, length)
 
     cur.execute('''INSERT OR IGNORE INTO Artist (name) 
         VALUES ( ? )''', ( artist, ) )
@@ -72,10 +83,27 @@ for entry in all:
         VALUES ( ?, ? )''', ( album, artist_id ) )
     cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
     album_id = cur.fetchone()[0]
-
+    
+    cur.execute('''INSERT OR IGNORE INTO Genre (name) 
+        VALUES ( ? )''', ( genre,  ) )
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+    genre_id = cur.fetchone()[0]
+    
     cur.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, len, rating, count) 
-        VALUES ( ?, ?, ?, ?, ? )''', 
-        ( name, album_id, length, rating, count ) )
+        (title, album_id, genre_id, len, rating, count) 
+        VALUES ( ?, ?, ?, ?, ?, ? )''', 
+        ( name, album_id, genre_id, length, rating, count ) )
+        
+conn.commit()
 
-    conn.commit()
+
+sql_command = """SELECT Track.title, Artist.name, Album.title, Genre.name 
+                    FROM Track JOIN Genre JOIN Album JOIN Artist 
+                    ON Track.genre_id = Genre.ID and Track.album_id = Album.id 
+                        AND Album.artist_id = Artist.id
+                    ORDER BY Artist.name LIMIT 3"""
+"""                    
+for row in cur.execute(sql_command):
+    print(str(row))
+"""
+cur.close()
